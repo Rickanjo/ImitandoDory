77fe60cb0757845dd2240494e5404ca681baf9d7
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-03-26 18:57:15 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Um programa que traduza o texto em baleiês, substituindo cada vogal por 3 cópias dela mesma.
+
+String Frase;
+
+Console.WriteLine("----Imitando a Dory----");
+
+Console.Write("\nDigite uma Frase:" );
+Frase = Console.ReadLine()!;
+
+String FraseEmBaleis = Frase
+.Replace("a", "aaa") 
+.Replace("A", "AAA") 
+.Replace("e", "eee") 
+.Replace("E", "EEE") 
+.Replace("i", "iii") 
+.Replace("I", "III") 
+.Replace("o", "ooo") 
+.Replace("O", "OOO") 
+.Replace("u", "uuu") 
+.Replace("U", "UUU");
+
+Console.WriteLine($"Em baleies:{FraseEmBaleis}");
